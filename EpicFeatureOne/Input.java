9310018c7065cb0fdff7210ba7ba483e326ee0f1
adityak714 <91271297+adityak714@users.noreply.github.com>
2021-09-24 02:22:58 +0200

    Add your 1.2 & 1.3 codes in the StartMenu.java file
    Try to maybe edit my StartMenu.java and add your codes that you guys wrote in the switch statement wherever it mentions, Akuen and Anton. Then you need to push the updated StartMenu.java file again and then the version will be updated for everyone, and we will be basically done with EF1. Last step is to get it reviewed with Greg.

---
+++ EpicFeatureOne/Input.java
@@ -0,0 +1,21 @@
+package com.ClothStoreProject.EpicFeatureOne;
+
+import java.util.Scanner;
+
+public class Input {
+// All methods to process input in our program go here
+
+    public static int response;
+
+    public static void askOption() {
+        Scanner input = new Scanner(System.in);
+        response = input.nextInt();
+        input.nextLine();
+    }
+
+    /*
+    Here we need to add Plan B steps for if the user enters a String, or any other unwanted type
+    instead of int i.e Error handling
+    */
+
+}
---
+++ EpicFeatureOne/MainEF1.java
@@ -0,0 +1,9 @@
+package com.ClothStoreProject.EpicFeatureOne;
+
+public class MainEF1 {
+
+    public static void main(String[] args) {
+        StartMenu.optionsList();
+        StartMenu.putInOption();
+    }
+}
