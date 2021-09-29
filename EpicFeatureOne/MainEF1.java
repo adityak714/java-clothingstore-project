18b9779c2853dc67f92620c1881c862d2709f882
Aditya Khadkikar <91271297+adityak714@users.noreply.github.com>
2021-09-29 13:21:19 +0200

    Delete EpicFeatureOne directory

--- EpicFeatureOne/MainEF1.java
+++
@@ -1,9 +0,0 @@
-package com.ClothStoreProject.EpicFeatureOne;
-
-public class MainEF1 {
-
-    public static void main(String[] args) {
-        StartMenu.optionsList();
-        StartMenu.putInOption();
-    }
-}
