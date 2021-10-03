18fdef76353b539337975ab137c2725a6c5ccfbe
adityak714 <guskhadad@student.gu.se>
2021-10-03 02:01:43 +0200

    did my part of EF2, 2.6 & 2.7

--- DIT043_Group23/src/main/java/facade/Facade.java
+++ DIT043_Group23/src/main/java/facade/Facade.java
@@ -1,24 +1,68 @@
 package facade;
 
+import non_businesslogic.CreateItem;
+import non_businesslogic.Menu;
 import non_businesslogic.MenuThree;
 
+import java.lang.reflect.Array;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Facade {
+
+    private ArrayList<String> ids = new ArrayList<>();
+    private ArrayList<String> names = new ArrayList<>();
+    private ArrayList<Double> prices = new ArrayList<>();
+
+    // TA
+    // Is it fine if variables are declared like this in the body of the facade?
+
+    public ArrayList<String> getIds() {return ids;}
+    public ArrayList<Double> getPrices() {return prices;}
+    public ArrayList<String> getNames() {return names;}
+
+    public Facade() {}
 
     // This class only has the skeleton of the methods used by the test.
     // You must fill in this class with your own code. You can (and should) create more classes
     // that implement the functionalities listed in the Facade and in the Test Cases.
+
     public String createItem(String itemID, String itemName, double unitPrice) {
+        ids.add(itemID);
+        names.add(itemName);
+        prices.add(unitPrice);
+
         return "";
     }
 
     public String printItem(String itemID) {
-        return "";
+        double price = 0;
+        String name = "";
+
+        do {
+            if (ids.contains(itemID)) {
+                double itemPrice = CreateItem.Create().getPrice();
+                name = CreateItem.Create().getName();
+
+                DecimalFormat truncate = new DecimalFormat("#.##");
+                truncate.setRoundingMode(RoundingMode.DOWN);
+                price = Double.parseDouble(truncate.format(itemPrice));
+
+                // TA
+                // Is this method of printing a single particular item workable?
+
+            } else {
+                System.out.println("Item" + itemID + " was not registered yet.");
+            }
+        } while (ids.contains(itemID));
+
+        return String.format(" %s: %s. " + price + " SEK ", itemID, name);
     }
 
     public String removeItem(String itemID) {
-        itemID = "";
         return "";
     }
 
@@ -135,7 +179,12 @@ public class Facade {
     }
 
     public String printAllItems() {
-        return "";
+        String fullItemsList = "All registered items. " + Menu.EOL;
+        String eachItem = String.format(" %s: %s. " + CreateItem.Create().getPrice() + " SEK ",
+                CreateItem.Create().getID(), CreateItem.Create().getName());
+
+        return fullItemsList + eachItem.repeat(ids.size());
+        // TA
     }
 
     public String printMostProfitableItems() {
