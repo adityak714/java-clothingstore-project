a8b7e3ed134db89cefe95668fb61313f1c7c44ae
d0nate110 <77355470+d0nate110@users.noreply.github.com>
2021-10-05 17:15:13 +0100

    ItemController

---
+++ DIT043_Group23/src/main/java/non_businesslogic/ItemController.java
@@ -0,0 +1,75 @@
+package non_businesslogic;
+
+
+import java.util.ArrayList;
+
+public class ItemController {
+
+
+private static ArrayList <Item> items = new ArrayList<Item>(); 
+
+
+//EF 2.1 I want to create items in my system 
+// so that I can sell them in my system.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public static boolean CreatingItem() {
+
+         
+    //Taking input from a user
+    String id = UserInput.inputString("\nType an ID number for the new item(4 digits): ");
+    String name = UserInput.inputString("Create a name for the new item: ");
+    double price = UserInput.inputDouble("Enter a price for the new item: ");
+    
+        //2.2 I want to avoid the creation of items with invalid data 
+        // so that I only have reliable data in my system.
+        if(id.isEmpty() || name.isEmpty() || price <= 0){
+    
+            System.out.println("Invalid data for item.");
+            return false;
+        }
+    
+        for(int i = 0; i < items.size(); i++){
+            //if(items[i] == null){
+                ///Item item =  new Item(id, name, price);
+                //allItems.add(item);
+                //System.out.println("Item is created. ");
+                
+                //return true;
+     
+            }
+        }
+        //System.out.print("Item cannot be created");
+        return false;
+        //QuitOrProceed();
+    
+        
+        }
+    
+    public static char QuitOrProceed(){
+       
+        char quitOrProceedInput = UserInput.inputChar("Press 'q' to quit to the previous menu. If you wish to proceed, press 'p'. ");
+        if(quitOrProceedInput == 'q'){
+            Menu.printMenu();
+        }
+        return quitOrProceedInput;
+        
+  
+    
+  */  
+}
+
